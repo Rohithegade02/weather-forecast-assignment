6e1a87ddf189f027a232cd2643c0d1bbb6582ceb
Rohit Hegade <60090023+Rohithegade02@users.noreply.github.com>
2024-04-15 22:21:31 +0530

    app/page iupdated

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,11 @@ import React from 'react';
 import MainLayout from "./MainLayout";
 import City from "./components/City";
 
-interface MainLayoutProps {
-  weatherdata: any;
-}
 
-const Home: React.FC<MainLayoutProps> = ({ weatherdata }) => {
+
+const Home: React.FC = () => {
   return (
-    <MainLayout weatherdata={weatherdata}>
+    <MainLayout >
       <City/>
     </MainLayout>
   );
